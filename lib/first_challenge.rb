d0c7a591038047e73d7a09d8cc976feba9751fe0
Lee Bardon <leerbardon@gmail.com>
2019-12-19 22:11:51 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,17 +1,4 @@
 epic_tragedy = {
-   :montague => {
-      :patriarch => {},
-      :matriarch => {},
-      :hero => {},
-      :hero_friends => []
-   },
-   :capulet => {
-      :patriarch => {},
-      :matriarch => {},
-      :heroine => {},
-      :heroine_friends => [
-          {},
-          {}
-      ]
-   }
+  :montague => {},
+  :capulet => {}
 }
